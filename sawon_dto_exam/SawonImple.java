a505170caf6d03fc018837ab14e3ea5f17d647b2
KMtheStarter <ikhong72@naver.com>
2019-07-08 11:39:22 +0900

    sawon_dto 예제

---
+++ sawon_dto_exam/SawonImple.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oracle2;
+
+import conn.ConnDriver;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author bit
+ */
+public class SawonImple implements SawonInter {
+
+    private static SawonImple sawondao;
+    public synchronized static SawonImple getSawondao(){
+        if (sawondao == null){
+            sawondao = new SawonImple();
+        }
+        return sawondao;
+    }            
+    
+    @Override
+    public List<SawonDTO> getListSawon(String searchValue) {
+
+        String sql1 = "select sabun, saname, deptno, sapay, sahire from sawon where saname like ? order by 1 desc";
+        Connection con = null;
+        PreparedStatement pstm = null;
+        // select일 경우 cursor를 반환
+        ResultSet rs = null;
+        // SawonDTO의 집합객체 생성
+        List<SawonDTO> list = new ArrayList<>();
+        
+        try {
+            // 1. connection 연결
+            con = ConnDriver.getMyConnection();
+            System.out.println("연결성공: " + con);
+            pstm = con.prepareStatement(sql1);
+            pstm.setString(1, "%" + searchValue + "%");
+            // sql문을 전송한 후 커서를 반환
+            rs = pstm.executeQuery();
+
+            while (rs.next()) {
+                // 사원의 정보 하나 당 SawonDTO가 하나씩 생성
+                SawonDTO vo = new SawonDTO();
+                // sabun, saname, deptno, sapay, sahire
+                vo.setSabun(rs.getInt("sabun"));
+                vo.setSaname(rs.getString("saname"));
+                vo.setDeptno(rs.getInt("deptno"));
+                vo.setSapay(rs.getInt("sapay"));
+                vo.setSahire(rs.getString("sahire"));
+                // 사원들의 정보를 리스트에 추가
+                list.add(vo);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try { // 자원해제
+                if (rs != null) {
+                    rs.close();
+                }
+                if (pstm != null) {
+                    pstm.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return list;
+    }
+
+}
